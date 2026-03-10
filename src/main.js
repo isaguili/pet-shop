8308151d585fda0ac011a37eed4be8760ca5014a
isaguili <silva.isabellagui@gmail.com>
2026-03-10 13:35:00 -0300

    style: apply styles to page elements

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/utils.css";
+import "./styles/schedule.css";
+import "./styles/input.css";
+import "./styles/form.css";
+import "./styles/button.css";
